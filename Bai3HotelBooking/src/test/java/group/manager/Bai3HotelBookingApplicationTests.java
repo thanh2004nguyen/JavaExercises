cc6a3f483b4afdda9e2b621493f0182b0586f827
thanh2004nguyen <116701025+thanh2004nguyen@users.noreply.github.com>
2025-03-12 22:50:45 +0700

    Add Bai3HotelManager

---
+++ Bai3HotelBooking/src/test/java/group/manager/Bai3HotelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package group.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bai3HotelBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
